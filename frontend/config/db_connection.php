c4bf58704b5cff16dea6181325cfdf534c1e38a0
sonyadriko <sonyadiadriko@gmail.com>
2025-01-15 15:07:09 +0700

    update db

--- frontend/config/db_connection.php
+++ frontend/config/db_connection.php
@@ -2,7 +2,7 @@
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'naive_bayes_3';
+$DATABASE_NAME = 'naive_bayes';
 
 $conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 // if (mysqli_connect_errno()) {
